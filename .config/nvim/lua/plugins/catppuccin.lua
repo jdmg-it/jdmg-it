14bc5db5269e571b1f87291a8ebdf307d45c76c1
José David Montoya <jdmg6@alu.ua.es>
2024-06-18 12:27:14 +0200

    Add transparency configurations nvim

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -1,11 +1,19 @@
 return {
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme "catppuccin-mocha"
-    end
-  }
+    {
+        "catppuccin/nvim",
+        lazy = false,
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                flavour = "mocha",
+                transparent_background = true,
+                integrations = {
+                    neotree = true,
+                    treesitter = true,
+                }
+            })
+            vim.cmd.colorscheme "catppuccin"
+        end
+    }
 }
